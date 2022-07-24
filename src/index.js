5267685bafa3832eed9d65a4ac06cb314f6105dd
mahmoud <mahmoud.d.p7@gmail.com>
2022-07-24 11:38:58 -0700

    added redux Dependencies

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap/dist/react-bootstrap.js";
 import { BrowserRouter } from "react-router-dom";
 import "./fonts/Yekan.ttf";
-
+import store from "./StateManagement/store";
+import { Provider } from "react-redux";
 //============================
 ReactDOM.render(
-  <BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </Provider>
+  ,
   document.getElementById("root")
 );
